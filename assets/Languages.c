8a9dbeb77c994452985b9c9d87e8fbc0f4278a08
Jorge Eremiev <jorgech3@gmail.com>
2025-01-09 16:12:08 -0500

    Indentation fixes.

--- assets/Languages.c
+++ assets/Languages.c
@@ -1,6 +1,7 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                              THIS FILE WAS AUTO-GENERATED - DO NOT EDIT                               //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////THIS FILE WAS AUTO-GENERATED - DO NOT EDIT //////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // INCLUDES
@@ -30,90 +31,90 @@ LangROMSpec* const _languages[] =
 
 const char* const EnglishLanguageStrings[] =
 {
-    /* kStringAutomaticPauseBody: */
-    "Please take a rest!",
-    /* kStringAutomaticPauseTitle: */
-    "AUTOMATIC PAUSE",
-    /* kStringContinue: */
-    "Continue",
-    /* kStringDeleteData: */
-    "Delete Data",
-    /* kStringMobileError: */
-    "10-000\nMobile adapter is not plugged in properly.\nPlease refer to the instruction manual and\nplug it in securely.",
-    /* kStringMobilePrepareCommunication: */
-    "Preparing for communication.",
-    /* kStringNewGame: */
-    "New Game",
-    /* kStringNo: */
-    "No",
-    /* kStringPrecautionScreenBody: */
-    "     IMPORTANT\nREAD INSTRUCTION AND\nPRECAUTION BOOKLETS\n  BEFORE OPERATING",
-    /* kStringTradingCard01Title: */
-    "Incubus",
-    /* kStringTradingCard02Title: */
-    "Cybil",
-    /* kStringTradingCard03Title: */
-    "Cheryl",
-    /* kStringTradingCard04Title: */
-    "Alessa",
-    /* kStringTradingCard05Title: */
-    "Dahlia",
-    /* kStringTradingCard06Title: */
-    "Kaufmann",
-    /* kStringTradingCard07Title: */
-    "Lisa",
-    /* kStringTradingCard08Title: */
-    "Mumbler",
-    /* kStringTradingCard09Title: */
-    "Air Screamer",
-    /* kStringTradingCard10Title: */
-    "Split Head",
-    /* kStringTradingCard11Title: */
-    "Puppet Nurse",
-    /* kStringTradingCard12Title: */
-    "Twinfeeler",
-    /* kStringTradingCard13Title: */
-    "Floatstinger",
-    /* kStringTradingCard14Title: */
-    "Groaner",
-    /* kStringTradingCard15Title: */
-    "Harry",
-    /* kStringTradingCard16Title: */
-    "Switchboard",
-    /* kStringTradingCard17Title: */
-    "Chemicals",
-    /* kStringTradingCard18Title: */
-    "Specimens",
-    /* kStringTradingCard19Title: */
-    "Telephone",
-    /* kStringTradingCard20Title: */
-    "Key",
-    /* kStringTradingCard21Title: */
-    "Cafe",
-    /* kStringTradingCard22Title: */
-    "Music Room",
-    /* kStringTradingCard23Title: */
-    "Hospital",
-    /* kStringTradingCard24Title: */
-    "Study",
-    /* kStringTradingCard25Title: */
-    "Aqueduct",
-    /* kStringTradingCard26Title: */
-    "Elevator",
-    /* kStringTradingCard27Title: */
-    "Cemetery",
-    /* kStringTradingCard28Title: */
-    "His Fate",
-    /* kStringTradingCard29Title: */
-    "Her Fate",
-    /* kStringTradingCard30Title: */
-    "First Sight",
-    /* kStringTradingCard31Title: */
-    "Escape",
-    /* kStringTradingCard32Title: */
-    "Love",
-    /* kStringYes: */
-    "Yes",
+	/* kStringAutomaticPauseBody: */
+	"Please take a rest!",
+	/* kStringAutomaticPauseTitle: */
+	"AUTOMATIC PAUSE",
+	/* kStringContinue: */
+	"Continue",
+	/* kStringDeleteData: */
+	"Delete Data",
+	/* kStringMobileError: */
+	"10-000\nMobile adapter is not plugged in properly.\nPlease refer to the instruction manual and\nplug it in securely.",
+	/* kStringMobilePrepareCommunication: */
+	"Preparing for communication.",
+	/* kStringNewGame: */
+	"New Game",
+	/* kStringNo: */
+	"No",
+	/* kStringPrecautionScreenBody: */
+	"     IMPORTANT\nREAD INSTRUCTION AND\nPRECAUTION BOOKLETS\n  BEFORE OPERATING",
+	/* kStringTradingCard01Title: */
+	"Incubus",
+	/* kStringTradingCard02Title: */
+	"Cybil",
+	/* kStringTradingCard03Title: */
+	"Cheryl",
+	/* kStringTradingCard04Title: */
+	"Alessa",
+	/* kStringTradingCard05Title: */
+	"Dahlia",
+	/* kStringTradingCard06Title: */
+	"Kaufmann",
+	/* kStringTradingCard07Title: */
+	"Lisa",
+	/* kStringTradingCard08Title: */
+	"Mumbler",
+	/* kStringTradingCard09Title: */
+	"Air Screamer",
+	/* kStringTradingCard10Title: */
+	"Split Head",
+	/* kStringTradingCard11Title: */
+	"Puppet Nurse",
+	/* kStringTradingCard12Title: */
+	"Twinfeeler",
+	/* kStringTradingCard13Title: */
+	"Floatstinger",
+	/* kStringTradingCard14Title: */
+	"Groaner",
+	/* kStringTradingCard15Title: */
+	"Harry",
+	/* kStringTradingCard16Title: */
+	"Switchboard",
+	/* kStringTradingCard17Title: */
+	"Chemicals",
+	/* kStringTradingCard18Title: */
+	"Specimens",
+	/* kStringTradingCard19Title: */
+	"Telephone",
+	/* kStringTradingCard20Title: */
+	"Key",
+	/* kStringTradingCard21Title: */
+	"Cafe",
+	/* kStringTradingCard22Title: */
+	"Music Room",
+	/* kStringTradingCard23Title: */
+	"Hospital",
+	/* kStringTradingCard24Title: */
+	"Study",
+	/* kStringTradingCard25Title: */
+	"Aqueduct",
+	/* kStringTradingCard26Title: */
+	"Elevator",
+	/* kStringTradingCard27Title: */
+	"Cemetery",
+	/* kStringTradingCard28Title: */
+	"His Fate",
+	/* kStringTradingCard29Title: */
+	"Her Fate",
+	/* kStringTradingCard30Title: */
+	"First Sight",
+	/* kStringTradingCard31Title: */
+	"Escape",
+	/* kStringTradingCard32Title: */
+	"Love",
+	/* kStringYes: */
+	"Yes",
 };
 
 const LangROMSpec EnglishLanguageSpec =
@@ -130,90 +131,90 @@ const LangROMSpec EnglishLanguageSpec =
 
 const char* const GermanLanguageStrings[] =
 {
-    /* kStringAutomaticPauseBody: */
-    "Bitte mache eine Pause!",
-    /* kStringAutomaticPauseTitle: */
-    "AUTOMATISCHE PAUSE",
-    /* kStringContinue: */
-    "Fortsetzen",
-    /* kStringDeleteData: */
-    "Daten L�schen",
-    /* kStringMobileError: */
-    "10-000\nMobil-Adapter ist nicht richtig eingesteckt.\nBitte lesen Sie die Anleitung und stecken \ndiesen sicher ein.",
-    /* kStringMobilePrepareCommunication: */
-    "Bereite Kommunikation vor.",
-    /* kStringNewGame: */
-    "Neues Spiel",
-    /* kStringNo: */
-    "Nein",
-    /* kStringPrecautionScreenBody: */
-    "        WICHTIG\nBITTE LESEN SIE VOR DEM\n SPIELEN DIE ANLEITUNG\nUND SICHERHEITSHINWEISE",
-    /* kStringTradingCard01Title: */
-    "Inkubus",
-    /* kStringTradingCard02Title: */
-    "Cybil",
-    /* kStringTradingCard03Title: */
-    "Cheryl",
-    /* kStringTradingCard04Title: */
-    "Alessa",
-    /* kStringTradingCard05Title: */
-    "Dahlia",
-    /* kStringTradingCard06Title: */
-    "Kaufmann",
-    /* kStringTradingCard07Title: */
-    "Lisa",
-    /* kStringTradingCard08Title: */
-    "Mumbler",
-    /* kStringTradingCard09Title: */
-    "Air Screamer",
-    /* kStringTradingCard10Title: */
-    "Split Head",
-    /* kStringTradingCard11Title: */
-    "Krankenschwester",
-    /* kStringTradingCard12Title: */
-    "Twinfeeler",
-    /* kStringTradingCard13Title: */
-    "Floatstinger",
-    /* kStringTradingCard14Title: */
-    "Groaner",
-    /* kStringTradingCard15Title: */
-    "Harry",
-    /* kStringTradingCard16Title: */
-    "Schalttafel",
-    /* kStringTradingCard17Title: */
-    "Chemikalien",
-    /* kStringTradingCard18Title: */
-    "Pr�parate",
-    /* kStringTradingCard19Title: */
-    "Telefon",
-    /* kStringTradingCard20Title: */
-    "Schl�ssel",
-    /* kStringTradingCard21Title: */
-    "Caf�",
-    /* kStringTradingCard22Title: */
-    "Musikraum",
-    /* kStringTradingCard23Title: */
-    "Krankenhaus",
-    /* kStringTradingCard24Title: */
-    "Untersuchung",
-    /* kStringTradingCard25Title: */
-    "Aqu�dukt",
-    /* kStringTradingCard26Title: */
-    "Aufzug",
-    /* kStringTradingCard27Title: */
-    "Friedhof",
-    /* kStringTradingCard28Title: */
-    "Sein Schicksal",
-    /* kStringTradingCard29Title: */
-    "Ihr Schicksal",
-    /* kStringTradingCard30Title: */
-    "Erster Blick",
-    /* kStringTradingCard31Title: */
-    "Flucht",
-    /* kStringTradingCard32Title: */
-    "Liebe",
-    /* kStringYes: */
-    "Ja",
+	/* kStringAutomaticPauseBody: */
+	"Bitte mache eine Pause!",
+	/* kStringAutomaticPauseTitle: */
+	"AUTOMATISCHE PAUSE",
+	/* kStringContinue: */
+	"Fortsetzen",
+	/* kStringDeleteData: */
+	"Daten L�schen",
+	/* kStringMobileError: */
+	"10-000\nMobil-Adapter ist nicht richtig eingesteckt.\nBitte lesen Sie die Anleitung und stecken \ndiesen sicher ein.",
+	/* kStringMobilePrepareCommunication: */
+	"Bereite Kommunikation vor.",
+	/* kStringNewGame: */
+	"Neues Spiel",
+	/* kStringNo: */
+	"Nein",
+	/* kStringPrecautionScreenBody: */
+	"        WICHTIG\nBITTE LESEN SIE VOR DEM\n SPIELEN DIE ANLEITUNG\nUND SICHERHEITSHINWEISE",
+	/* kStringTradingCard01Title: */
+	"Inkubus",
+	/* kStringTradingCard02Title: */
+	"Cybil",
+	/* kStringTradingCard03Title: */
+	"Cheryl",
+	/* kStringTradingCard04Title: */
+	"Alessa",
+	/* kStringTradingCard05Title: */
+	"Dahlia",
+	/* kStringTradingCard06Title: */
+	"Kaufmann",
+	/* kStringTradingCard07Title: */
+	"Lisa",
+	/* kStringTradingCard08Title: */
+	"Mumbler",
+	/* kStringTradingCard09Title: */
+	"Air Screamer",
+	/* kStringTradingCard10Title: */
+	"Split Head",
+	/* kStringTradingCard11Title: */
+	"Krankenschwester",
+	/* kStringTradingCard12Title: */
+	"Twinfeeler",
+	/* kStringTradingCard13Title: */
+	"Floatstinger",
+	/* kStringTradingCard14Title: */
+	"Groaner",
+	/* kStringTradingCard15Title: */
+	"Harry",
+	/* kStringTradingCard16Title: */
+	"Schalttafel",
+	/* kStringTradingCard17Title: */
+	"Chemikalien",
+	/* kStringTradingCard18Title: */
+	"Pr�parate",
+	/* kStringTradingCard19Title: */
+	"Telefon",
+	/* kStringTradingCard20Title: */
+	"Schl�ssel",
+	/* kStringTradingCard21Title: */
+	"Caf�",
+	/* kStringTradingCard22Title: */
+	"Musikraum",
+	/* kStringTradingCard23Title: */
+	"Krankenhaus",
+	/* kStringTradingCard24Title: */
+	"Untersuchung",
+	/* kStringTradingCard25Title: */
+	"Aqu�dukt",
+	/* kStringTradingCard26Title: */
+	"Aufzug",
+	/* kStringTradingCard27Title: */
+	"Friedhof",
+	/* kStringTradingCard28Title: */
+	"Sein Schicksal",
+	/* kStringTradingCard29Title: */
+	"Ihr Schicksal",
+	/* kStringTradingCard30Title: */
+	"Erster Blick",
+	/* kStringTradingCard31Title: */
+	"Flucht",
+	/* kStringTradingCard32Title: */
+	"Liebe",
+	/* kStringYes: */
+	"Ja",
 };
 
 const LangROMSpec GermanLanguageSpec =
