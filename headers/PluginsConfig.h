00219364bff819e2e4f6f70d7590ad91892a245b
Jorge Eremiev <jorgech3@gmail.com>
2025-01-14 22:51:29 -0500

    Regenerated files.

--- headers/PluginsConfig.h
+++ headers/PluginsConfig.h
@@ -1,37 +1,36 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                              THIS FILE WAS AUTO-GENERATED - DO NOT EDIT                               //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////  THIS FILE WAS AUTO-GENERATED - DO NOT EDIT  ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef PLUGINS_CONFIG_H_
 #define PLUGINS_CONFIG_H_
 
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// CONFIGURATION MACROS
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-//                                          CONFIGURATION MACROS                                          
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-
-#define __PLUGIN_LOW_POWER_ACTOR_USE_GUI_BGMAP_ANIMATED_SPRITE (false)
 #define __PLUGIN_LOW_POWER_ACTOR_X_POSITION (368)
 #define __PLUGIN_LOW_POWER_ACTOR_Y_POSITION (12)
 #define __PLUGIN_LOW_POWER_ACTOR_Z_POSITION (0)
 
 #define __PLUGIN_AUTOMATIC_PAUSE_TITLE_TEXT_FONT "Default"
 #define __PLUGIN_AUTOMATIC_PAUSE_BODY_TEXT_FONT "Default"
-#define __PLUGIN_AUTOMATIC_PAUSE_USE_LOW_POWER_ENTITY (false)
+#define __PLUGIN_AUTOMATIC_PAUSE_USE_LOW_POWER_ACTOR (false)
 
 #define __PLUGIN_SAVE_DATA_MANAGER_CRC_MASK (0xEDB88320)
 #define __PLUGIN_SAVE_DATA_MANAGER_CRC_CHECK_RANGE (512)
 #define __PLUGIN_SAVE_DATA_MANAGER_SAVE_STAMP "SILENTHL"
 #define __PLUGIN_SAVE_DATA_MANAGER_SAVE_STAMP_LENGTH (8)
 
-#define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_USE_LOW_POWER_ENTITY (true)
+#define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_USE_LOW_POWER_ACTOR (true)
 #define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_LOW_POWER_INDICATOR_X_POSITION (4)
 #define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_LOW_POWER_INDICATOR_Y_POSITION (1)
 #define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_LOW_POWER_ACTOR_X_POSITION (352)
 #define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_LOW_POWER_ACTOR_Y_POSITION (12)
 #define __PLUGIN_ADJUSTMENT_SCREEN_NINTENDO_LOW_POWER_ACTOR_Z_POSITION (0)
 
-#define __PLUGIN_PRECAUTION_SCREEN_USE_LOW_POWER_ENTITY (false)
+#define __PLUGIN_PRECAUTION_SCREEN_USE_LOW_POWER_ACTOR (false)
 #define __PLUGIN_PRECAUTION_SCREEN_TEXT_FONT "Silent"
+
 
 #endif
